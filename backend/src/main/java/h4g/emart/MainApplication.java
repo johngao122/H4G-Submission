26ccebaa489127f5230d17453cd89b8b34bbc822
Mooncheez <62787982+johngao122@users.noreply.github.com>
2025-01-15 10:47:48 +0800

    revert

--- backend/src/main/java/h4g/emart/MainApplication.java
+++ backend/src/main/java/h4g/emart/MainApplication.java
@@ -14,12 +14,6 @@ public class MainApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MainApplication.class, args);
-
-		System.out.println("Logging environment variables:");
-        System.out.println("MONGO_DATABASE: " + System.getenv("MONGO_DATABASE"));
-        System.out.println("MONGO_USER: " + System.getenv("MONGO_USER"));
-        System.out.println("MONGO_PASSWORD: " + System.getenv("MONGO_PASSWORD"));
-        System.out.println("MONGO_CLUSTER: " + System.getenv("MONGO_CLUSTER"));
 	}
 
 }
